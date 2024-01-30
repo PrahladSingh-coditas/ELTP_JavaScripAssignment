a70139ccf579492b78e7c82153c598d064eb5178
PrahladSingh-coditas <158143460+PrahladSingh-coditas@users.noreply.github.com>
2024-01-30 17:11:24 +0530

    Update Day2Assignment.js
    Modifications made as per suggestions from Akhil Sir

--- Day2Assignment.js
+++ Day2Assignment.js
@@ -14,23 +14,23 @@ let employeeAssetID;
 
 const employeeID = 8966;
 const employeeAccount = 'XXXX9078';
-let employeeDomain = "DevOps";
-var employeeBuddy = "XYZ";  //Not recommended to use var 
+const employeeDomain = "DevOps";
+const employeeBuddy = "XYZ";  //Not recommended to use var 
 
 //3. Declare variables to store your first name, last name, marital status, country and age in multiple lines
 
-let firstName;
-let lastName;
-let maritalStatus;
-let country;
-let age;
+const firstName;
+const lastName;
+const maritalStatus;
+const country;
+const age;
 //Note: We can't use const as we need to to assign value at the same line.
 //Note: var allows redeclaration of variables. We cant redeclare a let or const variable.
 
 
 //4. Declare variables to store your father name, mother name, and fatherAge in a single line
 
-var fatherName, motherName, fatherAge;
+const fatherName, motherName, fatherAge;
 //Note: Redecalaration is not allowed in let and const.
 
 
