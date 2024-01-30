f0dcabd77f6bf36b4cd238aa543ab16392a674cd
PrahladSingh-coditas <158143460+PrahladSingh-coditas@users.noreply.github.com>
2024-01-30 14:49:58 +0530

    Add files via upload

---
+++ Day2Assignment.js
@@ -0,0 +1,42 @@
+/*
+Theme: The below program is created with refernce to Employee realated Data a comapany.
+*/
+
+//1. Declare four variables without assigning values
+let joiningDate;
+let employeeSalary;
+let employeeSeatNumber;
+let employeeAssetID;
+//Note: We can't use const because it has to be assigned some data at the same line and var is depricated.
+
+
+//2.Declare four variables with assigned values
+
+const employeeID = 8966;
+const employeeAccount = 'XXXX9078';
+let employeeDomain = "DevOps";
+var employeeBuddy = "XYZ";  //Not recommended to use var 
+
+//3. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+var firstName;
+var lastName;
+var maritalStatus;
+var country;
+var age;
+//Note: We can't use const as we need to to assign value at the same line.
+//Note: var allows redeclaration of variables. We cant redeclare a let or const variable.
+
+
+//4. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+var firstName, lastName, maritalStatus, country, age;
+//Note: Redecalaration is not allowed in let and const.
+
+
+//5. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+const myAge = 21;
+const yourAge = 22;
+
+console.log("My age: "+myAge);
+console.log("Your Age: "+ yourAge);
